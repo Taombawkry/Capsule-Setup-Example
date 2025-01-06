fa99d1ca733839fb66b042387e228aa0b33b833a
Mario Jere <130987129+mariojere@users.noreply.github.com>
2025-01-06 19:34:36 +0000

    added Capsule button and api key for acsess

--- my-vue-app/src/utils/Capsule-Client-Instance.ts
+++ my-vue-app/src/utils/Capsule-Client-Instance.ts
@@ -2,7 +2,6 @@ import Capsule, { Environment } from "@usecapsule/react-sdk";
 
 const capsuleApiKey = import.meta.env.VITE_REACT_APP_CAPSULE_API_KEY;
 
-const capsule = new Capsule(Environment.BETA,capsuleApiKey);
+export const capsule = new Capsule(Environment.BETA,capsuleApiKey);
 
-// Verify the instance is created successfully
 console.log("Capsule instance created:", capsule);
