060ef8576e54c47efa374eaed67c31cfadb8ce73
Mario Jere <130987129+mariojere@users.noreply.github.com>
2025-01-06 19:51:42 +0000

    ethers setup added

--- my-vue-app/src/App.tsx
+++ my-vue-app/src/App.tsx
@@ -1,9 +1,36 @@
 import { useState } from "react";
 import { CapsuleModal } from "@usecapsule/react-sdk";
+import Capsule, { Environment } from "@usecapsule/react-sdk";
+import { CapsuleEthersSigner } from "@usecapsule/ethers-v6-integration";
+import { ethers } from "ethers";
+import { TransactionRequest } from "ethers";
+
 import "@usecapsule/react-sdk/styles.css";
-import { capsule } from "./utils/Capsule-Client-Instance";
 
 function App() {
+
+  const capsuleApiKey = import.meta.env.VITE_REACT_APP_CAPSULE_API_KEY;
+
+ const capsule = new Capsule(Environment.BETA,capsuleApiKey);
+
+// Set up the provider
+const provider = new ethers.JsonRpcProvider(YOUR_RPC_URL);
+
+// Create the Capsule Ethers Signer
+const ethersSigner = new CapsuleEthersSigner(capsule, provider);
+
+console.log("Capsule instance created:", capsule);
+  // Construct the transaction
+const transaction: TransactionRequest = {
+  from: "0x...", // Your address
+  to: "0x...", // Recipient address
+  value: ethers.parseUnits("0.1", "ether"),
+  // Configure other transaction parameters as needed
+};
+// Call the signTransaction method to sign the transaction
+const signedTx = async ()=>{
+  return await ethersSigner.signTransaction(transaction);
+} 
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
--- my-vue-app/src/utils/Capsule-Client-Instance.ts
+++
@@ -1,7 +0,0 @@
-import Capsule, { Environment } from "@usecapsule/react-sdk";
-
-const capsuleApiKey = import.meta.env.VITE_REACT_APP_CAPSULE_API_KEY;
-
-export const capsule = new Capsule(Environment.BETA,capsuleApiKey);
-
-console.log("Capsule instance created:", capsule);
